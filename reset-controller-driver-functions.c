dc5e2f2831c5e5814cf267f66d6183af3d1bf962
Romain Perier <romain.perier@gmail.com>
2015-11-24 07:59:18 +0100

    linux-reset-controller.tex: Fixed grammar and improved snippets of code to make them bigger in slides

--- reset-controller-driver-functions.c
+++ reset-controller-driver-functions.c
@@ -1,31 +1,22 @@
-#define MESON_HHI_GCLK_OFFSET 0x1050
-
+static struct reset_control_ops meson_reset_ops = {
+    .assert   = meson_reset_assert,
+    .deassert = meson_reset_deassert,
+};
 int meson_reset_assert(struct reset_controller_dev *rcdev,
-                              unsigned long id)
+                       unsigned long id)
 {
-    int bank = id / BITS_PER_LONG;
-    int bit = id % BITS_PER_LONG;
-    struct meson_reset_data *priv = container_of(rcdev,
-                                        struct meson_reset_data, rcdev);
-    u32 reg;
-
-    reg = readl(priv->reset_base + MESON_HHI_GCLK_OFFSET + bank);
+    /* Select the right bank and compute your offset */
+    [...]
+    u32 reg = readl(priv->reset_base + offset);
     reg |= BIT(bit);
-    writel(reg, priv->reset_base + MESON_HHI_GCLK_OFFSET + bank);
+    writel(reg, priv->reset_base + offset);
     return 0;
 }
-
-static int meson_reset_deassert(struct reset_controller_dev *rcdev,
-                                unsigned long id)
+static int meson_reset_deassert(struct reset_controller_dev *rcdev, unsigned long id)
 {
-    int bank = id / BITS_PER_LONG;
-    int bit = id % BITS_PER_LONG;
-    struct meson_reset_data *priv = container_of(rcdev,
-                                        struct meson_reset_data, rcdev);
-    u32 reg;
-
-    reg = readl(priv->reset_base + MESON_HHI_GCLK_OFFSET + bank);
+    [...]
+    u32 reg = readl(priv->reset_base + offset);
     reg &= ~BIT(bit);
-    writel(reg, priv->reset_base + MESON_HHI_GCLK_OFFSET + bank);
+    writel(reg, priv->reset_base + offset);
     return 0;
 }
--- reset-controller-driver-probe.c
+++ reset-controller-driver-probe.c
@@ -1,9 +1,3 @@
-/* This driver is still under development, it might contains mistakes :)*/
-static struct reset_control_ops meson_reset_ops = {
-    .assert   = meson_reset_assert,
-    .deassert = meson_reset_deassert,
-};
-
 static int meson_reset_probe(struct platform_device *pdev)
 {
     struct meson_reset_data *priv;
@@ -11,7 +5,6 @@ static int meson_reset_probe(struct platform_device *pdev)
     priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
     if (!priv)
         return -ENOMEM;
-    /* Map physical memory region to low memory and do other stuffs */
     [...]
     priv->rcdev.owner = THIS_MODULE;
     priv->rcdev.nr_resets = 4 * 32;
@@ -20,12 +13,9 @@ static int meson_reset_probe(struct platform_device *pdev)
     priv->rcdev.of_reset_n_cells = 1;
     return reset_controller_register(&priv->rcdev);
 }
-
 static int meson_reset_remove(struct platform_device *pdev)
 {
     struct meson_reset_data *priv = platform_get_drvdata(pdev);
-
     reset_controller_unregister(&priv->rcdev);
     return 0;
 }
-
